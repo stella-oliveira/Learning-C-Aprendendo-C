0a5788f8b91aa0636b80e2ac27bc44ae84bd7083
stella-oliveira <74307418+stella-oliveira@users.noreply.github.com>
2020-11-13 11:18:37 -0300

    Update Aula 07 - Matrizes.c

--- Aula 07 - Matrizes.c
+++ Aula 07 - Matrizes.c
@@ -1,9 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
 	
 	int ra[5], i, j;
-	
 	float notas[5][4];		// notas[linhas][colunas] = 5 linhas (5 alunos) e 4 colunas (4 notas)
 	
 	for(i=0; i<5; i++){
@@ -16,14 +15,13 @@ int main() {
 		
 		for(j=0; j<4; j++){					// i percorre linhas e j percorre colunas		
 			printf("Informe a nota %i: ", j+1);
-			scanf("%f", &notas[i][j]); 		// Tem que ter duas variáveis de controle, porque senão ele vai ler notas[0][0], 
-		}									// depois notas[1][1], etc -> para cada linha, coluna vai de 0 a 3	
+			scanf("%f", &notas[i][j]); 			// Tem que ter duas variÃ¡veis de controle, porque senÃ£o ele vai ler notas[0][0], 
+		}							// depois notas[1][1], etc -> para cada linha, coluna vai de 0 a 3	
 	}
 	
 	printf("\n")
 	
 	for(i=0; i<5; i++){		// Imprimir matriz
-		
 		for(j=0; j<4; j++){						
 			printf("%.2f ", notas[i][j]);
 		}								
