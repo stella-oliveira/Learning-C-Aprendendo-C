a62564be4382d7ffaecbea5b8ab7791bde26d949
stella-oliveira <74307418+stella-oliveira@users.noreply.github.com>
2020-11-13 11:11:04 -0300

    Update Aula 03 - Desafio FOR.c

--- Aula 03 - Desafio FOR.c
+++ Aula 03 - Desafio FOR.c
@@ -1,29 +1,21 @@
-// 6 cartas para serem entregues
-// 2 carteiros
+/* 6 cartas para serem entregues por 2 carteiros
+NÃºmeros pares ou Ã­mpares: ler os nÃºmeros de n1 a n6
+	Se for par -> carteiro 1
+	SenÃ£o -> carteiro 2
+Quantas cartas cada carteiro vai entregar?
+Ex: 4 cartas para o carteiro 1 e 2 cartas para o carteiro 2 */
 
-// par ou ímpar
-
-// ler os números de n1 a n6
-
-// se for par -> carteiro1
-// senao -> carteiro 2
-
-// quantas cartas cada carteiro vai entregar?
-
-// resposta: 4 cartas para o carteiro 1 e 2 cartas para o carteiro 2
-
-#include<stdio.h>
+#include <stdio.h>
 #include <conio.h>
 #include <locale.h>
 
 int main(){
-	
 	setlocale(LC_ALL, "Portuguese");
 	
 	int carteiro1=0, carteiro2=0, n1, x;
 	
 	for(x=1; x<7; x++){
-		printf("\nInforme o número da residência: ");
+		printf("\nInforme o nÃºmero da residÃªncia: ");
 		scanf("%i", &n1);
 		
 		if (n1%2==0){
@@ -33,6 +25,6 @@ int main(){
 		}
 	}
 	
-	printf("\nA divisão de cartas é:\n\t%i cartas para o Carteiro 1 (residências pares)\n\t%i cartas para o Carteiro 2 (residências ímpares).", carteiro1, carteiro2);
+	printf("\nA divisÃ£o de cartas Ã©:\n\t%i cartas para o Carteiro 1 (residÃªncias pares)\n\t%i cartas para o Carteiro 2 (residÃªncias Ã­mpares).", carteiro1, carteiro2);
 	
 }
