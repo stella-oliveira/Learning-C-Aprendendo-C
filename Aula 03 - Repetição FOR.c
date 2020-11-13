84965af865570a7f7d58de663b463e1670ee0ccb
stella-oliveira <74307418+stella-oliveira@users.noreply.github.com>
2020-11-13 11:13:01 -0300

    Update Aula 03 - Repetição FOR.c

--- Aula 03 - Repetição FOR.c
+++ Aula 03 - Repetição FOR.c
@@ -1,31 +1,30 @@
-// Algoritmo lê X números e mostra a média deles ao final
+// Algoritmo lÃª X nÃºmeros e mostra a mÃ©dia deles ao final
 
 #include <stdio.h>
 #include <locale.h>
 
 int main (){
-	
 	setlocale(LC_ALL, "Portuguese");
 	
 	int i, n, soma;
 	float media;
 	
-	soma = 0; // Para não ter lixo de memória, começar com 0
+	soma = 0; // Para nÃ£o ter lixo de memÃ³ria, comeÃ§ar com 0
 	
-	// for -> quando sabe quantidade de repetições
+	// for -> quando sabe quantidade de repetiÃ§Ãµes
 	// while
 	// do...while
 	
-	for(i=1;i<=10;i++){ 						//	Estrutura de repetição
-		printf("Informe um número: ");			//	i vai inicializar laço (valor inicial do laço)
-		scanf("%i", &n);						//	Qual a condição (até onde vai)
-												//	Qual é o controle/passo (vai de um em um) 
+	for(i=1;i<=10;i++){ 					//	Estrutura de repetiÃ§Ã£o
+		printf("Informe um nÃºmero: ");			//	i vai inicializar laÃ§o (valor inicial do laÃ§o)
+		scanf("%i", &n);				//	Qual a condiÃ§Ã£o (atÃ© onde vai)
+								//	Qual Ã© o controle/passo (vai de um em um) 
 		soma += n; // soma = soma + n	
 }
 	
-	media = (float)soma/10; // (float) é um cast, força o resultado a ser mostrado como float
-							// Entende que é inteiro dividido por inteiro e não mostra casas decimais
-							// Outra solução: dividir por 10.0 (entende como float) ou declarar SOMA como float
+	media = (float)soma/10; // (float) Ã© um cast, forÃ§a o resultado a ser mostrado como float
+				// Entende que Ã© inteiro dividido por inteiro e nÃ£o mostra casas decimais
+				// Outra soluÃ§Ã£o: dividir por 10.0 (entende como float) ou declarar SOMA como float
 	
-	printf("\nA média é: %.2f", media);
+	printf("\nA mÃ©dia Ã©: %.2f", media);
 }
